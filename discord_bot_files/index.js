0feb6ce4eb2c4d31d068be5735bcb7d3d668edbc
LiquidInteract <79763184+LiquidInteract@users.noreply.github.com>
2021-03-06 00:47:18 +0000

    Make sure these are all in a folder

---
+++ discord_bot_files/index.js
@@ -0,0 +1,69 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const token = 'Your bot token goes here';
+client.login(token);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+//command info 1, this is the code that the bot will use and read to be able to send the message in chat
+//remove from here (if you dont want -info to credit)
+client.on('message', msg => {
+    if (msg.content === '-github') {
+      msg.reply('This was made with a custom template from LiquidInteracts github');
+    }
+})
+//to here
+client.on('message', msg => {
+    if (msg.content === '-help') {
+      msg.reply('-github shows the github where the custom template was made ');
+    }
+})
+
+client.on('message', msg => {
+    if (msg.content === '-version') {
+      msg.reply('-Version: 1.0.0');
+    }
+})
+
+client.on('message', msg => {
+    if (msg.content === '-help') {
+      msg.reply('-version : Shows the version of the bot');
+    }
+})
+
+//add more lines here for more commands
+
+client.on('message', message => {
+  if (message.channel.type -= 'text' || message.author.bot)
+    return;
+
+  let command = message.content.split(' ')[0].slice(1);
+  let args = message.content.replace('.' + command, '').trim();
+  //broken ping comand
+
+  switch (command) {
+    case 'ping': {
+      message.channel.send('Pong! (~ ' + client.ping + 'ms)');
+      break;
+    }
+
+
+    case 'uptime': {
+//started uptime checks
+//use -uptime to show the uptime
+      let days = Math.floor(client.uptime / 86400000);
+      let hours = Math.floor(client.uptime / 3600000) % 24;
+      let minutes = Math.floor(client.uptime / 60000) % 60;
+      let seconds = Math.floor(client.uptime / 1000) % 60;
+
+      message.channel.send(`BOT Uptime:\n${days}d ${hours}h ${minutes}m ${seconds}s
+`);
+
+      
+      break;
+    }
+  }
+});
---
+++ discord_bot_files/start.bat
@@ -0,0 +1,4 @@
+@echo
+title Discord BOT
+echo if you see this the batch file has been loaded
+node index.js
